f1c428ded83594168d0a86d1cec957c0887c9a40
Robert Rice <rrice1291@gmail.com>
2018-09-18 19:47:43 -0500

    adding first bootstrap files

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('I loaded');
+const toDoInputElem = document.getElementById("toDoInput");
+const notesInputElem = document.getElementById("notesInput");
+
+const submitToDoButtonElem = document.getElementById("submitToDo");
+
+const printToDom = (stringToPrint, whereToPrint) => {
+    document.getElementById(whereToPrint).innerHTML += stringToPrint;
+}
+
+const buildNewToDoCard = (toDo, notes) => {
+let domString = `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${toDo}</h5>
+  <p class="card-text">${notes}</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>`;
+
+    printToDom(domString, 'toDoCards');
+}
+
+submitToDoButtonElem.addEventListener("click", (e) => {
+    e.preventDefault();//prevents chrome from erasing our console logs
+
+buildNewToDoCard(toDoInputElem.value, notesInputElem.value);
+});
